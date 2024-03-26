1ff17b35afd112beb3586d5ac9e2d5e39db328ac
kotten <kotten@iu.edu>
2024-03-26 19:49:06 -0400

    Adding top bar to search bar component

---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SearchBar(props) {
+    return (
+        <>
+            <div>
+                <p>ravenous</p>
+            </div>
+        </>
+    )
+}
+
+export default SearchBar;
