8d7c1e8670f43199315abe6eede92c37eb428d43
kotten <kotten@iu.edu>
2024-03-26 19:15:59 -0400

    Removing filled in business from Business.js and adding props variables

--- src/Business/Business.js
+++ src/Business/Business.js
@@ -3,15 +3,15 @@ import React from 'react';
 function Business(props) {
     return (
         <>
-            <img src="https://www.foodandwine.com/thmb/3kzG4PWOAgZIIfZwMBLKqoTkaGQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/margherita-pizza-with-argula-and-prosciutto-FT-RECIPE0721-04368ec288a84d2e997573aca0001d98.jpg"/>
-            <p>Papas Pizzeria</p>
-            <p>421 E Third St</p>
-            <p>Bloomington</p>
-            <p>IN</p>
-            <p>47401</p>
-            <p>Italian</p>
-            <p>4.5</p>
-            <p>11</p>
+            <img src={props.info.image}/>
+            <p>{props.info.name}</p>
+            <p>{props.info.address}</p>
+            <p>{props.info.city}</p>
+            <p>{props.info.state}</p>
+            <p>{props.info.zip}</p>
+            <p>{props.info.type}</p>
+            <p>{props.info.rating}</p>
+            <p>{props.info.reviews}</p>
         </>
     );
 }
