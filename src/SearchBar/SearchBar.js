7052f1f60d7c1e0e65432f6b41aab11d60f2fd39
kotten <kotten@iu.edu>
2024-03-27 13:48:25 -0400

    Adding filters to search bar component

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -8,7 +8,11 @@ function SearchBar(props) {
                 <p className={styles.ravenous}>ravenous</p>
             </div>
             <div className={styles.searchDiv}>
-
+                <div className={styles.filters}>
+                    <p>Best Match</p>
+                    <p>Highest Rated</p>
+                    <p>Most Reviewed</p>
+                </div>
             </div>
         </>
     )
