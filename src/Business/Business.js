8fe2ec3f55982f8716fc178f4af72f016f9361a5
kotten <kotten@iu.edu>
2024-03-29 13:41:37 -0400

    Styling business component

--- src/Business/Business.js
+++ src/Business/Business.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import styles from './Business.module.css';
 
 function Business(props) {
     return (
-        <>
-            <img src={props.info.image}/>
-            <p>{props.info.name}</p>
-            <p>{props.info.address}</p>
-            <p>{props.info.city}</p>
-            <p>{props.info.state}</p>
-            <p>{props.info.zip}</p>
-            <p>{props.info.type}</p>
-            <p>{props.info.rating}</p>
-            <p>{props.info.reviews}</p>
-        </>
+        <div className={styles.businessDiv}>
+            <img src={props.info.image} className={styles.image}/>
+            <p className={styles.name}>{props.info.name}</p>
+            <div className={styles.infoDiv}>
+                <div className={styles.addressDiv}>
+                    <p className={styles.address}>{props.info.address}</p>
+                    <p className={styles.address}>{props.info.city}</p>
+                    <div className={styles.stateZip}>
+                        <p className={styles.address}>{props.info.state}</p>
+                        <p className={styles.address}>{props.info.zip}</p>
+                    </div>
+                </div>
+                <div className={styles.rightDiv}>
+                    <p className={styles.type}>{props.info.type}</p>
+                    <p className={styles.rating}>{props.info.rating} stars</p>
+                    <p className={styles.reviews}>{props.info.reviews} reviews</p>
+                </div>
+            </div>
+        </div>
     );
 }
 
