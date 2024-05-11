f4cebdf8e8ec6102b3b1f52c3a2a5fbbc91c0c7f
krisotten <kristenlotten@gmail.com>
2024-05-11 15:23:03 -0400

    Turning json response into an array to be used for business list

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,18 +3,31 @@ const base_url = 'https://api.yelp.com/v3';
 
 function yelpSearch(term, location, option) {
     const cors = 'https://cors-anywhere.herokuapp.com/';
-    const endPoint = '/business/search';
+    const endPoint = '/businesses/search';
     const queryParams = `?location=${location}&term=${term}&sort_by=${option}&limit=5`;
     const url = cors + base_url + endPoint + queryParams;
-
-    console.log(url);
     
     fetch(url, {headers:{Authorization: `Bearer ${api_key}`}}).then(response => {
         if (response.ok) {
             return response.json();
         }
         throw new Error('Request failed');
-    })
+    }).then(jsonResponse => {
+        if (jsonResponse.businesses) {
+            return jsonResponse.businesses.map(business => ({
+                id: business.id,
+                img: business.image_url,
+                name: business.name,
+                address: business.location.address1,
+                city: business.location.city,
+                state: business.location.state,
+                zip: business.location.zip_code,
+                type: business.categories[0].title,
+                rating: business.rating,
+                reviews: business.review_count
+            }));
+        } 
+    });
 }
 
 export default yelpSearch;
