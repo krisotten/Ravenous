db98a19bfe378417242553b9674184226d81a000
krisotten <kristenlotten@gmail.com>
2024-05-11 14:43:37 -0400

    Adding yelp api functionality to api.js

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
-import styles from './SearchBar.module.css'
+import styles from './SearchBar.module.css';
+import yelpSearch from '../utils/api.js';
 
 function SearchBar(props) {
     const [filter, setFilter] = useState(null);
@@ -17,11 +18,14 @@ function SearchBar(props) {
     function handleFilterChange(e) {
         var newFilter = e.target.innerHTML;
         newFilter = newFilter.replace(' <br>', '');
-        setFilter(newFilter);
+        if (newFilter == "Best Match") {setFilter('best_match');}
+        else if (newFilter == 'Highest Rated') {setFilter('rating');}
+        else {setFilter('review_count');}   
     }
 
     function handleButtonClick(e) {
         console.log(`Searching Yelp with ${search}, ${location}, ${filter}`);
+        console.log(yelpSearch(search, location, filter));
     }
 
     return (
@@ -31,9 +35,9 @@ function SearchBar(props) {
             </div>
             <div className={styles.searchDiv}>
                 <div className={styles.filters}>
-                    <p className={filter==='Best Match' ? styles.filterActive : styles.filter} onClick={handleFilterChange} >Best <br /> Match</p>
-                    <p className={filter==='Highest Rated' ? styles.filterActive : styles.filter} onClick={handleFilterChange}>Highest <br /> Rated</p>
-                    <p className={filter==='Most Reviewed' ? styles.filterActive : styles.filter} onClick={handleFilterChange}>Most <br /> Reviewed</p>
+                    <p className={filter==='best_match' ? styles.filterActive : styles.filter} onClick={handleFilterChange} >Best <br /> Match</p>
+                    <p className={filter==='rating' ? styles.filterActive : styles.filter} onClick={handleFilterChange}>Highest <br /> Rated</p>
+                    <p className={filter==='review_count' ? styles.filterActive : styles.filter} onClick={handleFilterChange}>Most <br /> Reviewed</p>
                 </div>
                 <div className={styles.searchBars}>
                     <input type="text" className={styles.searchBar} placeholder='Search Businesses' onChange={handleSearchChange} value={search}/>
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,1 +1,20 @@
 const api_key = 'WIViuH6d7-yYfnfT8Nwn2G7hoNFCFKOS-ycvgSnaCy8QQhs3uFOuEkAgCkURXZZ98jf1nCu-D0y8glC_6LPXp4_gwYzvK1W0cb70a0DvqIJs5PcdWBw-MYyHTZo_ZnYx';
+const base_url = 'https://api.yelp.com/v3';
+
+function yelpSearch(term, location, option) {
+    const cors = 'https://cors-anywhere.herokuapp.com/';
+    const endPoint = '/business/search';
+    const queryParams = `?location=${location}&term=${term}&sort_by=${option}&limit=5`;
+    const url = cors + base_url + endPoint + queryParams;
+
+    console.log(url);
+    
+    fetch(url, {headers:{Authorization: `Bearer ${api_key}`}}).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Request failed');
+    })
+}
+
+export default yelpSearch;
